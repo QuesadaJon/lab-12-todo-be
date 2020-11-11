e4892261c566c53a1ba6f794cf74f7b29c1f2c58
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-11-10 17:52:34 -0800

    created todos.js

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'walk the dog',
+    completed: false,
+  },
+  {
+    name: 'wash the car',
+    completed: false,
+  },
+  {
+    name: 'change the oil',
+    completed: false,
+  }
+];
